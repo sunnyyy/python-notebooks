df7fbbade548ec322637226ffb408105fe4c647b
Sravana Reddy <sravana@cs.dartmouth.edu>
2017-01-25 17:30:15 -0500

    tutorial files and installaton checker

---
+++ README.md
@@ -0,0 +1,6 @@
+# Problem Set 0
+
+Follow [the instructions in the problem set](http://wellesleynlp.github.io/machinelearning/ps0.html) to install Python, the required libraries, and Git.
+
+Clone this repository and run the code locally. 
+You need not fork this repository unless you want to write new code and commit it for your own personal use. 
---
+++ check.py
@@ -0,0 +1,29 @@
+error = "{0} not installed. Need help? Post to Piazza, talk to classmates, \
+or come to office hours."
+success = "{0} is installed. Yay!"
+
+try:
+    import numpy
+    print success.format('NumPy')
+except:
+    print error.format('NumPy')
+
+try:
+    import scipy
+    print success.format('SciPy')
+except:
+    print error.format('SciPy')
+
+try:
+    import matplotlib
+    print success.format('Matplotlib')
+except:
+    print error.format('Matplotlib')
+
+try:
+    import sklearn
+    print success.format('Scikit-learn')
+except:
+    print error.format('Scikit-learn')
+
+
